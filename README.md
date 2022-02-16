b30aadbb744ab7ca75cb9f60eba8f41b528c32eb
Matthias Stahl <matthias.stahl@spiegel.de>
2022-02-16 18:04:34 +0100

    add SizeDetector.svelte

--- README.md
+++ README.md
@@ -17,3 +17,27 @@ export let type = 'iframe';
 // disable component (if true, no alignment takes place)
 export let disable = false;
 ```
+
+
+### [SizeDetector.svelte](./SizeDetector.svelte)
+
+Determine the size of an element without using Svelte iFrames and `bind:clientWidth` etc.
+
+Good for apps that will be embedded directly.
+
+*Properties*
+```{JavaScript}
+// width and height can be bound from outside
+export let width = undefined;
+export let height = undefined;
+```
+
+*Usage*
+```{JavaScript}
+<SizeDetector
+    bind:width={width}
+    bind:height={height}
+>
+    <div>stuff to be measured</div>
+</SizeDetector>
+```
---
+++ SizeDetector.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let width = undefined;
+    export let height = undefined;
+
+    let tid;
+    let container;
+
+    function handleResize() {
+        clearTimeout(tid);
+        ({ width, height } = container.getBoundingClientRect());
+    }
+
+    function throttleResize() {
+        clearTimeout(tid);
+        tid = setTimeout(handleResize, 100);
+    }
+
+    onMount(() => {
+        setTimeout(() => {
+            window.dispatchEvent(new Event('resize'));
+            throttleResize();
+        }, 125);
+
+        new ResizeObserver(throttleResize).observe(container);
+
+        return () => clearTimeout(tid);
+    });
+</script>
+
+<svelte:window on:resize={throttleResize} />
+
+<div
+    class="size-detector"
+    bind:this={container}
+>
+    <slot />
+</div>
+
+<style lang="scss">
+    .size-detector {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+</style>
